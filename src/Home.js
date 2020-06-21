2dae86e86b31314c166a557454345cc39bd987db
unknown <ryanmurphycorp@protonmail.comm>
2020-06-21 10:42:24 -0700

    changes header of homepage

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function Profile() {
   return (
     <div>
-      <h1 class="headertext">Ryan Murphy, Tech Writer</h1>
+      <h1 class="headertext">Ryan Murphy</h1>
     </div>
   );
 }
