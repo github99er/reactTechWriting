e73adb274912d9d608526eacae9fe2d8515870ad
github99er <ryanmurphycorp@protonmail.comm>
2021-01-11 15:17:16 -0800

    Updates resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import MyResume from "./RMSept.pdf";
+import MyResume from "./Ryan_Murphy_Resume_Google.pdf";
 
 export default function Resume() {
   return (
